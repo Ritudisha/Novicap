b8ddc291657ae25a6a806d990670b85ae4267855
Ritudisha <ritudisha@simplifiedautomation.ca>
2020-02-24 10:45:08 +0530

    Added invoice logic

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Novicap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var invoice = new Invoice();
+            Console.WriteLine("Your Total Amount is : " + invoice.GetInvoice(args.ToList(), @"..\Novicap\ProductConfig.json"));
+        }
+    }
+}
